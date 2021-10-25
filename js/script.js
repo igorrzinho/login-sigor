0bb5eef72c4094347d4e3bc784a54e8d3dd9dc62
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2021-10-25 00:55:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 var name = document.getElementById('username').value;
 var password = document.getElementById('password').value;
+var modalUsername = document.getElementById('modal-username')
+var modalReset = document.getElementById('modal-reset');
 
 function send(){
     if((name == '')||(password == '')){
-        window.alert('escreva seu nome e sua senha');
+        modalUsername.classList.add("show-modal");
     }
 }
 
 function forgot(){
-    window.prompt('enviaremos uma validação para seu email \ndigite-o:')
+    modalReset.classList.add("show-modal");
+}
+
+function closeUsername(){
+    modalUsername.classList.remove("show-modal");
+}
+
+function closeReset(){
+    modalReset.classList.remove("show-modal");
 }
